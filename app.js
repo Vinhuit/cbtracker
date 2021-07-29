a70f2431cc3625bfa2b6b4ff10d6a2328f93b3a4
Vinh Huynh <caubequay00@gmail.com>
2021-07-29 18:11:00 +0700

    Update app.js

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use((err, req, res, next) => {
 });
 
 const httpServer = http.createServer(app);
-httpServer.listen(port, () => {
+httpServer.listen(80, () => {
   console.log('HTTPS Server running on port 3000');
 });
 
